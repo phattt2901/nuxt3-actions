279feae58d520a17d0df3d8a57903c21ac6944ec
phattt2901 <phattt2901@gmail.com>
2023-12-07 09:55:44 +0700

    New Update

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-  <h1>Hello, My friends !</h1>
+    <h1>Hello, My friends </h1>
+    <h2 class="title">This is a function</h2>
+    <p class="description">This is a function</p>
   </div>
 </template>
